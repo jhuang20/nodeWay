32c92498d92eb197bed35d82a67a0225ce0a8a5b
james huang <jhuang20@stuy.edu>
2018-04-18 08:39:37 -0400

    interface added

--- Deque.java
+++ Deque.java
@@ -1,11 +1,13 @@
 public interface Deque<Card> {
+  private DLLNode<Card> _list;
   private int _size;
-  public void addFirst(Card e);
+  public void addFirst(Card e);//adds to beginning
 
-  public void addLast(Card e);
+  public void addLast(Card e);//adds to end
 
-  public Card peekFirst();
-  public Card peekLast();
-  public Card removeFirst();
-  public Card removeLast();
+  public Card peekFirst();//peeks at element being removed in front
+  public Card peekLast();//peeks at element being removed in back
+  public Card removeFirst();//remove first element
+  public Card removeLast();//remove last element
+  public int size();
 }
